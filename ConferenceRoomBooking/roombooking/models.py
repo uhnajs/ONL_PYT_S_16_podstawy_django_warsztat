f51cc2f6b7d706813600c25f51decd502b16cd02
uhnajs <117354@student.san.edu.pl>
2024-02-12 22:58:38 +0100

    added models and views

--- ConferenceRoomBooking/roombooking/models.py
+++ ConferenceRoomBooking/roombooking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.IntegerField()
+    projector_availability = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- ConferenceRoomBooking/roombooking/views.py
+++ ConferenceRoomBooking/roombooking/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
-# Create your views here.
+def add_room(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RoomForm()
+    return render(request, 'add_room.html', {'form': form})
